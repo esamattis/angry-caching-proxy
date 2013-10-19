3adf65ebcae9891bbdbd5cadd3c228a0b905dd47
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-10-20 01:17:49 +0300

    Clean up triggers

--- buildin-triggers.js
+++ buildin-triggers.js
@@ -2,15 +2,13 @@
 module.exports = {
 
     "apt-get": function isDebRequest(req, res) {
-
         // Cache all archive.ubuntu.com, fi.archive.ubuntu.com etc. requests
         if (!/^.*archive.ubuntu.com$/.test(req.headers.host)) return false;
 
-        // Example:
         // http://archive.ubuntu.com/ubuntu/pool/main/g/git/git-core_1.7.10.4-1ubuntu1_all.deb
-        if (/^.*\.deb$/.test(req.url)) {
-            return true;
-        }
+        var deb = /^.*\.deb$/;
+
+        return deb.test(req.url);
     },
 
 
@@ -18,12 +16,10 @@ module.exports = {
 
         if (req.headers.host !== "registry.npmjs.org") return false;
 
-
         // Node modules are in .tgz files
-        // Example: http://registry.npmjs.org/boom/-/boom-0.4.2.tgz
-        if (/^.*\.tgz$/.test(req.url)) {
-            return true;
-        }
+        // http://registry.npmjs.org/boom/-/boom-0.4.2.tgz
+        var tgz = /^.*\.tgz$/;
+        return tgz.test(req.url);
 
     },
 
@@ -37,15 +33,12 @@ module.exports = {
 
         if (!hosts[req.headers.host]) return false;
 
-        // Example: http://rubygems.org/gems/actionmailer-3.2.14.gem
-        if (/^.*\.gem$/.test(req.url)) {
-            return true;
-        }
-
+        // http://rubygems.org/gems/actionmailer-3.2.14.gem
+        var gem = /^.*\.gem$/;
         // http://rubygems.org/quick/Marshal.4.8/eventmachine-1.0.3-java.gemspec.rz
-        if (/^.*\.gemspec\.rz/.test(req.url)) {
-            return true;
-        }
+        var gemspec = /^.*\.gemspec\.rz/;
+
+        return gem.test(req.url) || gemspec.test(req.url);
     },
 
     "pypi": function isPypiRequest(req, res) {
@@ -57,8 +50,8 @@ module.exports = {
 
         // http://pypi.python.org/packages/source/P/Products.DCWorkflow/Products.DCWorkflow-2.3.0-beta.tar.gz
         // http://pypi.python.org/packages/source/P/Products.PloneTestCase/Products.PloneTestCase-0.9.17.zip
-        var eggUrl = /^.*packages\/source\/.*(\.tar\.gz|\.zip)$/;
-        return eggUrl.test(req.url);
+        var egg = /^.*packages\/source\/.*(\.tar\.gz|\.zip)$/;
+        return egg.test(req.url);
 
     }
 
